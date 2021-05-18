e5dcd7e371b156ef189f0fe56d94d389da25ab25
as <>
2021-05-18 12:59:28 +0900

    Rayを飛ばすスクリプトを作成

---
+++ Assets/MyScript/Raycast.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class Raycast : MonoBehaviour
+{
+    private ARRaycastManager m_RaycastManager;
+
+    private List<ARRaycastHit> hitResults = new List<ARRaycastHit>();
+
+    [SerializeField]
+    private GameObject characterPrefab;
+    
+    void Start()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                if (m_RaycastManager.Raycast(touch.position, hitResults, TrackableType.PlaneWithinPolygon))
+                {
+                    Pose hitPose = hitResults[0].pose;
+                    GameObject character = Instantiate(characterPrefab, hitPose.position, hitPose.rotation);
+                }
+            }
+        }
+    }
+}
